d7f4b2d085fe5cf0c0ad7c9e9c75087fd3c48de0
Thiago Barbosa <thiagobarbosa48@gmail.com>
2021-05-17 15:26:49 -0300

    Preparing test data

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
 
   has_many :order_products
 
-  validates :name, :price, presence :true
+  validates :name, :price, presence: true
 
-  has_one_attached: image
+  has_one_attached :image
 end
